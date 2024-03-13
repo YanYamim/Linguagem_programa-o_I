ddb0d4501b036375432271dc4d3f2912af32abdd
Yan Yamim <119015786+YanYamim@users.noreply.github.com>
2024-03-13 19:51:26 -0300

    Update Macaco.java

--- Ativ. 7-03-2024/Macaco.java
+++ Ativ. 7-03-2024/Macaco.java
@@ -38,40 +38,5 @@ public class Macaco {
         macaco3.comer();
         macaco3.escalar();
         macaco3.comunicar();
-
-        Macaco macaco4 = new Macaco("Orangotango ", "Laranja", "Médio", "Onívoro");
-        macaco4.comer();
-        macaco4.escalar();
-        macaco4.comunicar();
-
-        Macaco macaco5 = new Macaco("Macaco-Aranha ", "Preto", "Médio", "Onívoro");
-        macaco5.comer();
-        macaco5.escalar();
-        macaco5.comunicar();
-
-        Macaco macaco6 = new Macaco("Macaco-Prego ", "Marrom", "Pequeno", "Onívoro");
-        macaco6.comer();
-        macaco6.escalar();
-        macaco6.comunicar();
-
-        Macaco macaco7 = new Macaco("Bugio ", "Marrom ou Preto", "Pequeno", "Onívoro");
-        macaco7.comer();
-        macaco7.escalar();
-        macaco7.comunicar();
-
-        Macaco macaco8 = new Macaco("Gorila ", "Preto", "Grande", "Herbívoro");
-        macaco8.comer();
-        macaco8.escalar();
-        macaco8.comunicar();
-
-        Macaco macaco9 = new Macaco("Babuíno ", "Branco ou Marrom", "Médio", "Onívoro");
-        macaco9.comer();
-        macaco9.escalar();
-        macaco9.comunicar();
-
-        Macaco macaco10 = new Macaco("Mico-Leão-Dourado ", "Laranja ou Preto", "Pequeno", "Onívoro");
-        macaco10.comer();
-        macaco10.escalar();
-        macaco10.comunicar();
     }
 }
