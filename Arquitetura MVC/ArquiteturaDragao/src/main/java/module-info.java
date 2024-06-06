83450bcc4c0699cc1cf30df1d7eeb92980d97ff6
Yan Yamim <119015786+YanYamim@users.noreply.github.com>
2024-06-05 21:43:36 -0300

    Delete Arquitetura MVC directory

--- Arquitetura MVC/ArquiteturaDragao/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.arquiteturadragao {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.arquiteturadragao to javafx.fxml;
-    exports org.example.arquiteturadragao;
-}
--- Arquitetura MVC/ArquiteturaMacaco/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.arquiteturamacaco {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.arquiteturamacaco to javafx.fxml;
-    exports org.example.arquiteturamacaco;
-}
